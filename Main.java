aad9711c35d2a33e0a1aac8146517c85a28e991f
Viktor Sergeyevich Yeliseyev <103144967+m0rganSw@users.noreply.github.com>
2023-02-10 01:20:41 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+package org.example;
+//package ru.yandex.praktikum;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class) // Напиши аннотацию для параметризованных тестов
+public class CheckIsAdultTest {
+
+    private final int age;
+    private final boolean result;
+
+    public CheckIsAdultTest(int age, boolean result) {
+        this.age = age;
+        this.result = result; // Инициализируй поля класса в конструкторе
+    }
+
+    @Parameterized.Parameters // Пометь метод аннотацией для параметров
+    public static Object[][] getTextData() {
+        return new Object[][] {
+                {18, true},
+                {17, false},
+                {20, true},
+                {21, true} // Заполни массив тестовыми данными и ожидаемым результатом
+        };
+    }
+
+    @Test
+    public void checkIsAdultWhenAgeThenResult() {
+        Program program = new Program();
+        // Передай сюда возраст пользователя
+        boolean isAdult = program.checkIsAdult(age);
+        // Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+        assertEquals("Ошибка", result, isAdult);
+    }
+}
+//Git не хочет работать СВОЛОЧ
